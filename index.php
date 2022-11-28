14b528d53b2e456576597755c9d5814863803b07
Yuri-Punzo <yuripunzo@hotmail.it>
2022-11-28 15:03:27 +0100

    esercizio completato

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $hotels = [
     ]
 ];
 
-foreach ($hotels as $hotel) {
+/* foreach ($hotels as $hotel) {
     echo $hotel["name"] . "<br>";
     echo $hotel["description"] . "<br>";
     if ($hotel["parking"] === true) {
@@ -61,6 +61,63 @@ foreach ($hotels as $hotel) {
     echo $hotel["vote"] . "<br>";
     echo $hotel["distance_to_center"] . "<br>";
     echo "<br>";
-}
+} */
 
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>PHP Hotels</title>
+</head>
+
+<body>
+    <div class="table-responsive">
+        <table class="table table-primary">
+            <?php foreach ($hotels as $hotel) : ?>
+                <thead>
+                    <tr>
+                        <th scope="col">
+                            <?php echo $hotel["name"] ?>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="">
+                        <td scope="row"><?php echo $hotel["description"] ?></td>
+                    </tr>
+                    <tr class="">
+                        <td scope="row">
+                            <?php if ($hotel["parking"] === true) {
+                                echo "Possibilità di parcheggio: Si" . "<br>";
+                            } else {
+                                echo "Possibilità di parcheggio: No" . "<br>";
+                            } ?>
+                        </td>
+                    </tr>
+                    <tr class="">
+                        <td scope="row">
+                            <?php echo "Voto: " . $hotel["vote"] ?>
+                        </td>
+                    </tr>
+                    <tr class="">
+                        <td scope="row">
+                            <?php echo "Distanza dal centro: " . $hotel["distance_to_center"] ?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="py-3"></td>
+                    </tr>
+                </tbody>
+            <?php endforeach ?>
+        </table>
+    </div>
+</body>
+
+</html>
